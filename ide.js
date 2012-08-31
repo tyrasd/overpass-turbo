0a531a045f311eff1d05a1ef345100c54e09f8ad
tyr <tyr.asd@gmail.com>
2012-08-31 16:16:40 +0200

    deleted too much, oops

--- ide.js
+++ ide.js
@@ -208,151 +208,8 @@ var ide = new(function() {
       },
     });
     ide.map.addControl(new SearchBox());
-  }
-  var overpassJSON2geoJSON = function(json) {
-    // 2. sort elements
-    var nodes = new Array();
-    var ways  = new Array();
-    var rels  = new Array();
-    for (var i=0;i<json.elements.length;i++) {
-      switch (json.elements[i].type) {
-        case "node":
-          nodes.push(json.elements[i]);
-          break;
-        case "way":
-          ways.push(json.elements[i]);
-          break;
-        case "relation":
-          rels.push(json.elements[i]);
-          break;
-        default:
-          alert("???");
-      }
-    }
+  } // init()
 
-    // 3. some data processing (e.g. filter nodes only used for ways)
-    var nids = new Object();
-    var nodeids = new Array();
-    for (var i=0;i<nodes.length;i++) {
-      nids[nodes[i].id] = nodes[i];
-      nodeids.push(nodes[i].id);
-    }
-    var poinids = new Array();
-    for (var i=0;i<nodes.length;i++) {
-      if (typeof nodes[i].tags != 'undefined')
-        poinids.push(nodes[i].id);
-    }
-    var waynids = new Array();
-    var wayids = new Array();
-    for (var i=0;i<ways.length;i++) {
-      wayids.push(ways[i].id);
-      for (var j=0;j<ways[i].nodes.length;j++) {
-        waynids.push(ways[i].nodes[j]);
-        ways[i].nodes[j] = nids[ways[i].nodes[j]];
-      }
-    }
-    var pois = new Array();
-    for (var i=0;i<nodes.length;i++) {
-      if ((waynids.indexOf(nodes[i].id) == -1) || // not related to any way
-          (poinids.indexOf(nodes[i].id) != -1))   // or has tags
-        pois.push(nodes[i]);
-    }
-    var relids = new Array();
-    for (var i=0;i<rels.length;i++) {
-      relids.push(rels[i].id);
-      for (var j=0;j<rels[i].members.length;j++) {
-        switch (rels[i].members[j].type) {
-        case "node":
-          n = nodeids.indexOf(rels[i].members[j].ref);
-          if (n != -1) {
-            if (typeof nodes[n].relations == "undefined")
-              nodes[n].relations = new Array();
-            nodes[n].relations.push({
-              "rel" : rels[i].id,
-              "role" : rels[i].members[j].role,
-            });
-          }
-        break;
-        case "way":
-          w = wayids.indexOf(rels[i].members[j].ref);
-          if (w != -1) {
-            if (typeof ways[w].relations == "undefined")
-              ways[w].relations = new Array();
-            ways[w].relations.push({
-              "rel" : rels[i].id,
-              "role" : rels[i].members[j].role,
-            });
-          }
-        break;
-        default:
-        }
-      }
-    }
-
-    // 4. construct geojson
-    var geojson = new Array();
-    var geojsonnodes = {
-      "type"     : "FeatureCollection",
-      "features" : new Array()};
-    for (i=0;i<pois.length;i++) {
-      geojsonnodes.features.push({
-        "type"       : "Feature",
-        "properties" : {
-          "tags" : pois[i].tags,
-          "relations" : pois[i].relations,
-        },
-        "id"         : pois[i].id,
-        "geometry"   : {
-          "type" : "Point",
-          "coordinates" : [pois[i].lon, pois[i].lat],
-        }
-      });
-    }
-    geojson.push(geojsonnodes);
-    var geojsonways = {
-      "type"     : "FeatureCollection",
-      "features" : new Array()};
-    for (var i=0;i<ways.length;i++) {
-      ways[i].tainted = false;
-      coords = new Array();
-      for (j=0;j<ways[i].nodes.length;j++) {
-        if (typeof ways[i].nodes[j] == "object")
-          coords.push([ways[i].nodes[j].lon, ways[i].nodes[j].lat]);
-        else
-          ways[i].tainted = true;
-      }
-
-      var way_type = "LineString"; // default
-      if (ways[i].nodes[0] == ways[i].nodes[ways[i].nodes.length-1]) {
-        if (typeof ways[i].tags != "undefined")
-          if ((typeof ways[i].tags["landuse"] != "undefined") ||
-              (typeof ways[i].tags["building"] != "undefined") ||
-              (typeof ways[i].tags["leisure"] != "undefined") ||
-              (typeof ways[i].tags["area"] == "yes") ||
-              ($.inArray(ways[i].tags["natural"], new Array("forest","wood","water"))) ||
-              false) {
-             way_type="Polygon";
-             coords = [coords];
-           }
-      }
-      geojsonways.features.push({
-        "type"       : "Feature",
-        "properties" : {
-          "tainted" : ways[i].tainted,
-          "tags" : ways[i].tags,
-          "relations" : ways[i].relations,
-        },
-        "id"         : ways[i].id,
-        "geometry"   : {
-          "type" : way_type,
-          "coordinates" : coords,
-        }
-      });
-    }
-    geojson.push(geojsonways);
-
-    return geojson;
-  }
   var make_combobox = function(input, options) {
     if (input[0].is_combobox)
       return;
@@ -374,7 +231,7 @@ var ide = new(function() {
       input.focus();
     });
     input[0].is_combobox = true;
-  }
+  } // make_combobox()
 
   // == public methods ==
 
