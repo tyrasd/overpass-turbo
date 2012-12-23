ab0574272a9832f1f49eaf60a9824170e63853ce
tyr <tyr.asd@gmail.com>
2012-12-23 13:50:25 +0100

    added getCenter() for simple polygons. from https://plus.google.com/108220049326938062364/posts/beV4PQYM58x

--- libs/geo.js
+++ libs/geo.js
@@ -28,3 +28,27 @@ function PointInPolygon(p,Q) {
     t *= CrossProdTest(Q,P[i],P[i+1]);
   return t;
 }
+
+L.Polygon.prototype.getCenter = function() {
+  var pts = this._latlngs;
+  var off = pts[0];
+  var twicearea = 0;
+  var x = 0;
+  var y = 0;
+  var nPts = pts.length;
+  var p1,p2;
+  var f;
+  for (var i = 0, j = nPts - 1; i < nPts; j = i++) {
+    p1 = pts[i];
+    p2 = pts[j];
+    f = (p1.lat - off.lat) * (p2.lng - off.lng) - (p2.lat - off.lat) * (p1.lng - off.lng);
+    twicearea += f;
+    x += (p1.lat + p2.lat - 2 * off.lat) * f;
+    y += (p1.lng + p2.lng - 2 * off.lng) * f;
+  }
+  f = twicearea * 3;
+  return new L.LatLng(
+    lat: x / f + off.lat,
+    lng: y / f + off.lng
+  );
+}
