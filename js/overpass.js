2b59f44cff18aba4485f11077ef84c000f2df364
tyr <tyr.asd@gmail.com>
2013-09-01 12:58:18 +0200

    harmonize colouring of simple/normal multipolygons:
    show them all in the "relation member" colour, for consistency.

--- js/overpass.js
+++ js/overpass.js
@@ -204,7 +204,7 @@ setTimeout(function() {
           +"area {color:#03f; width:2; opacity:0.7; fill-color:#fc0; fill-opacity:0.3;} \n"
           // style modifications
           // objects in relations
-          +"relation node, relation way, relation relation {color:#d0f;} \n"
+          +"relation node, relation way, relation {color:#d0f;} \n"
           // tainted objects
           +"way:tainted, relation:tainted {dashes:5,8;} \n"
           // placeholder points
