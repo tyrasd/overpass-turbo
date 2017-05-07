603977f8537b1311df228033101bb43d3f2e44d5
Martin Raifer <tyr.asd@gmail.com>
2017-05-07 19:06:21 +0200

    fix logic in previous commit
    still for https://github.com/tyrasd/overpass-turbo/pull/283#issuecomment-299713211

--- js/ide.js
+++ js/ide.js
@@ -1796,7 +1796,7 @@ var ide = new(function() {
     // todo: more shortcuts
   }
   this.update_map = function() {
-    ide.waiter.open(i18n.t("waiter.processing_query") || "processing query...");
+    ide.waiter.open(i18n.t("waiter.processing_query") !== "/missing translation/" ? i18n.t("waiter.processing_query") : "processing query...");
     ide.waiter.addInfo("resetting map");
     $("#data_stats").remove();
     // resets previously highlighted error lines
