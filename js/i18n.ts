8bb5f7dd18ce1507a0bf844b1b3daa12984a2623
Simon Legner <Simon.Legner@gmail.com>
2022-10-23 21:23:13 +0200

    Convert i18n to ES6 class

--- js/i18n.ts
+++ js/i18n.ts
@@ -4,65 +4,75 @@ import _ from "lodash";
 
 import settings from "./settings";
 
-const i18n = new (function () {
-  function browser_locale() {
-    /* taken from https://github.com/maxogden/browser-locale by Max Ogden, BSD licensed */
-    let lang;
+function browser_locale() {
+  /* taken from https://github.com/maxogden/browser-locale by Max Ogden, BSD licensed */
+  let lang;
 
-    if (navigator.languages) {
-      // chrome does not currently set navigator.language correctly https://code.google.com/p/chromium/issues/detail?id=101138
-      // but it does set the first element of navigator.languages correctly
-      lang = navigator.languages[0];
-    } else if (navigator.userLanguage) {
-      // IE only
-      lang = navigator.userLanguage;
-    } else {
-      // as of this writing the latest version of firefox + safari set this correctly
-      lang = navigator.language;
-    }
-
-    return lang;
+  if (navigator.languages) {
+    // chrome does not currently set navigator.language correctly https://code.google.com/p/chromium/issues/detail?id=101138
+    // but it does set the first element of navigator.languages correctly
+    lang = navigator.languages[0];
+  } else if (navigator.userLanguage) {
+    // IE only
+    lang = navigator.userLanguage;
+  } else {
+    // as of this writing the latest version of firefox + safari set this correctly
+    lang = navigator.language;
   }
 
-  const default_lng = "en";
-  const languages = {
-    // translations found in locale/*.json
-    en: "English",
-    ca: "Catalan",
-    cs: "Czech",
-    da: "Danish",
-    eo: "Esperanto",
-    de: "German",
-    el: "Greek",
-    es: "Spanish",
-    et: "Estonian",
-    fr: "French",
-    gl: "Galician",
-    hr: "Croatian",
-    hu: "Hungarian",
-    it: "Italian",
-    ja: "Japanese",
-    lv: "Latvian",
-    nl: "Dutch",
-    no: "Norwegian",
-    pl: "Polish",
-    pt: "Portuguese",
-    "pt-BR": "Portuguese (Brazil)",
-    ru: "Russian",
-    sl: "Slovenian",
-    uk: "Ukrainian",
-    vi: "Vietnamese",
-    "zh-CN": "Chinese (Simplified)",
-    "zh-TW": "Chinese (Taiwan)"
-  };
-  const supported_lngs = _.keys(languages);
-  this.getSupportedLanguages = function () {
+  return lang;
+}
+
+const default_lng = "en";
+const languages = {
+  // translations found in locale/*.json
+  en: "English",
+  ca: "Catalan",
+  cs: "Czech",
+  da: "Danish",
+  eo: "Esperanto",
+  de: "German",
+  el: "Greek",
+  es: "Spanish",
+  et: "Estonian",
+  fr: "French",
+  gl: "Galician",
+  hr: "Croatian",
+  hu: "Hungarian",
+  it: "Italian",
+  ja: "Japanese",
+  lv: "Latvian",
+  nl: "Dutch",
+  no: "Norwegian",
+  pl: "Polish",
+  pt: "Portuguese",
+  "pt-BR": "Portuguese (Brazil)",
+  ru: "Russian",
+  sl: "Slovenian",
+  uk: "Ukrainian",
+  vi: "Vietnamese",
+  "zh-CN": "Chinese (Simplified)",
+  "zh-TW": "Chinese (Taiwan)"
+};
+
+type Language = keyof typeof languages;
+const supported_lngs: Language[] = _.keys(languages);
+
+export default class i18n {
+  // translated texts
+  static td: Record<string, string> = {};
+
+  static t(key: string) {
+    return this.td[key] || "/missing translation/";
+  }
+
+  static getSupportedLanguages() {
     return supported_lngs;
-  };
-  this.getSupportedLanguagesDescriptions = function () {
+  }
+  static getSupportedLanguagesDescriptions() {
     return languages;
-  };
-  this.getLanguage = function (lng) {
+  }
+  static getLanguage(lng: Language | string): Language {
     lng = lng || settings.ui_language;
     if (lng == "auto") {
       // get user agent's language
@@ -79,14 +89,13 @@ const i18n = new (function () {
       if ($.inArray(lng, supported_lngs) == -1) lng = lng.replace(/-.*/, "");
     }
     return lng;
-  };
-
+  }
   /**
    * Determines the language, fetches the language pack and translates the UI
    * @return <Promise>
    */
-  this.translate = function (lng) {
-    lng = i18n.getLanguage(lng);
+  static translate(lng: Language) {
+    lng = this.getLanguage(lng);
 
     if ($.inArray(lng, supported_lngs) == -1) {
       console.log(
@@ -99,8 +108,8 @@ const i18n = new (function () {
     try {
       return import(`../locales/${lng}.json`).then(
         (data) => {
-          Object.assign(td, data.default);
-          i18n.translate_ui();
+          Object.assign(this.td, data.default);
+          this.translate_ui();
           // todo: nicer implementation
           return data.default;
         },
@@ -109,38 +118,29 @@ const i18n = new (function () {
     } catch (e) {
       console.log(`failed to load language file ${lng}`, e);
     }
-  };
-  this.translate_ui = function (element) {
+  }
+  static translate_ui(element?: string) {
     // if a DOM object is provided, only translate that one, otherwise
     // look for all object with the class "t"
     $(element || ".t").each((nr, element) => {
-      // get translation term(s)
-      let terms = $(element).attr("data-t");
-      terms = terms.split(";");
-      for (let i = 0; i < terms.length; i++) {
-        const term = terms[i];
-        const tmp = term.match(/^(\[(.*)\])?(.*)$/);
-        const what = tmp[2];
-        const key = tmp[3];
-        let val = i18n.t(key);
-        const shortcut = $(element).attr("data-shortcut");
-        if (shortcut) val += ` [${shortcut}]`;
-        if (what === "html") {
-          $(element).html(val);
-        } else if (what !== undefined) {
-          $(element).attr(what, val);
-        } else {
-          $(element).text(val);
+      $(element || ".t").each((nr, element) => {
+        // get translation term(s)
+        const terms = $(element).attr("data-t")?.split(";") || [];
+        for (let i = 0; i < terms.length; i++) {
+          const term = terms[i];
+          const [, , what, key] = term.match(/^(\[(.*)\])?(.*)$/)!;
+          let val = this.t(key);
+          const shortcut = $(element).attr("data-shortcut");
+          if (shortcut) val += ` [${shortcut}]`;
+          if (what === "html") {
+            $(element).html(val);
+          } else if (what !== undefined) {
+            $(element).attr(what, val);
+          } else {
+            $(element).text(val);
+          }
         }
-      }
+      });
     });
-  };
-  this.t = function (key) {
-    return td[key] || "/missing translation/";
-  };
-
-  // translated texts
-  const td = {};
-})(); // end create i18n object
-
-export default i18n;
+  }
+}
