be0f345230357303450ad0ba7841e431b9faadc3
Martin Raifer <martin@raifer.tech>
2024-05-11 21:34:58 +0200

    fix test

--- tests/test.permalink.ts
+++ tests/test.permalink.ts
@@ -8,7 +8,7 @@ describe("ide.permalink", () => {
       '<!--\nThis is an example Overpass query.\nTry it out by pressing the Run button above!\nYou can find more examples with the Load tool.\n-->\n<query type="node">\n  <has-kv k="amenity" v="drinking_water"/>\n  <bbox-query {{bbox}}/><!--this is auto-completed with the\n                   current map view coordinates.-->\n</query>\n<print/>';
     const p = ide.compose_share_link(q, false, false, false);
     expect(p).to.eql(
-      "?Q=%3C%21--%0AThis+is+an+example+Overpass+query.%0ATry+it+out+by+pressing+the+Run+button+above%21%0AYou+can+find+more+examples+with+the+Load+tool.%0A--%3E%0A%3Cquery+type%3D%22node%22%3E%0A++%3Chas-kv+k%3D%22amenity%22+v%3D%22drinking_water%22%2F%3E%0A++%3Cbbox-query+%7B%7Bbbox%7D%7D%2F%3E%3C%21--this+is+auto-completed+with+the%0A+++++++++++++++++++current+map+view+coordinates.--%3E%0A%3C%2Fquery%3E%0A%3Cprint%2F%3E"
+      "?Q=%3C%21--%0AThis%20is%20an%20example%20Overpass%20query.%0ATry%20it%20out%20by%20pressing%20the%20Run%20button%20above%21%0AYou%20can%20find%20more%20examples%20with%20the%20Load%20tool.%0A--%3E%0A%3Cquery%20type%3D%22node%22%3E%0A%20%20%3Chas-kv%20k%3D%22amenity%22%20v%3D%22drinking_water%22%2F%3E%0A%20%20%3Cbbox-query%20%7B%7Bbbox%7D%7D%2F%3E%3C%21--this%20is%20auto-completed%20with%20the%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20current%20map%20view%20coordinates.--%3E%0A%3C%2Fquery%3E%0A%3Cprint%2F%3E"
     );
     expect(p).not.to.have.string("?q=");
     expect(p).not.to.have.string("&C=");
