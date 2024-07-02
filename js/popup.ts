bdac21db8f8d0069b1c518476b444ef60851e6d4
Martin Raifer <martin@raifer.tech>
2024-07-02 14:29:22 +0200

    fix link geneartion
    and update test snapshot – not sure why this was necessary, though ¯\_(ツ)_/¯

--- js/popup.ts
+++ js/popup.ts
@@ -68,11 +68,13 @@ export function featurePopupContent(feature: GeoJSON.Feature) {
         (k.match(/(^|:)wikipedia$/) &&
           (wiki_lang = v.match(/^([a-zA-Z]+):(.*)$/)) &&
           (wiki_page = wiki_lang[2]))
-      )
+      ) {
         wiki_page = wiki_page.replace(/#.*$/, (match) =>
+          // 'Target page#Section header' -> 'Target page#Section_header'
           match.replace(/ /g, "_")
-        ); // 'Target page#Section header' -> 'Target page#Section_header'
-      v = `<a href="//${wiki_lang[1]}.wikipedia.org/wiki/${wiki_page}" target="_blank">${v}</a>`;
+        );
+        v = `<a href="//${wiki_lang[1]}.wikipedia.org/wiki/${wiki_page}" target="_blank">${v}</a>`;
+      }
       // hyperlinks for wikidata entries
       if (k.match(/(^|:)wikidata$/))
         v = v.replace(
